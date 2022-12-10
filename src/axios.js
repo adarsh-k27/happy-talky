27f568f07e2018678d98b9144cad6c156c9c6b11
adarsh-k27 <adarshkdev27@gmail.com>
2022-12-10 19:34:54 +0530

    heroku fixed

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-    baseURL: "https://happy-t-alky-chat-app.herokuapp.com/"
+    baseURL: "https://happy-talky-app-site.onrender.com/"
 })
 
 export default axiosInstance;
